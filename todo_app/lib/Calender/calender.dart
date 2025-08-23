eee4d1a45ed58c1014a304ab419d2f15fb01f8c2
Yasin Khan <yasinkhan2856@gmail.com>
2025-08-22 18:13:36 -0700

    calender text editing

--- todo_app/lib/Calender/calender.dart
+++ todo_app/lib/Calender/calender.dart
@@ -75,7 +75,7 @@ class _CalenderState extends State<Calender> {
       backgroundColor: Color(0xff05243E),
       appBar: AppBar(
         title: const Text(
-          "Manage Your Time and date and time yasinkhan",
+          "Manage Your Time and date and time yasin khan",
           style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
